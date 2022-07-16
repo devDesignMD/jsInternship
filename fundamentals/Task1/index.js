0e12b7e3d9ee4232c152428b10955eb5462bcd0b
Ionella <ionellapb27@gmail.com>
2022-07-17 00:35:26 +0300

    git

--- fundamentals/Task1/index.js
+++ fundamentals/Task1/index.js
@@ -1,10 +1,45 @@
-let n = 3
-
-switch (n){
-    case 3 :
-        alert('three');
-        break;
-    case 4 :
-        alert('four');
-        break;
+//for
+for (let i = 0; i < 5; i++ ){
+    console.log(i)
 }
+
+//while
+let k = 0;
+while (k < 5){
+    k++
+    console.log(k)
+}
+
+
+//for in
+
+let num = [1,2,3,4];
+
+for (let element in num) {
+    console.log(element);
+}
+
+// for of
+let n = ['1', '2', '3'];
+
+for (let element of n) {
+    console.log(element);
+}
+
+//forEach
+let a = ["1", "2", "3"];
+a.forEach(function(entry) {
+    console.log(entry);
+});
+
+//map
+let arr =[3,4,5,6];
+
+let modifiedArr=
+    arr.map(function(element){
+        return element*3;
+    });
+console.log(modifiedArr)
+
+
+'for простой споcоб вывести массив'
