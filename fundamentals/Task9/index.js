0d9163b648f9a5a066ec38fc5169cf7f2829d643
Ionella <ionellapb27@gmail.com>
2022-07-17 01:16:11 +0300

    git

--- fundamentals/Task9/index.js
+++ fundamentals/Task9/index.js
@@ -1,9 +1,6 @@
-let Name = prompt("What your name?",'');
-alert (`Привет ${Name}`);
-
-let know = prompt("Do you know js?",'');
-if (know == 'Yes'){
-    alert('Good')
-}else {
-    alert('Do you want know?')
+const array = [1, 'a', 2, null, 3, undefined, 4, {}, 5, 6, 'word', 7, 8, false, 9, 100]
+function filter(num) {
+    for (let i = 0; num > 0; i++)
+        return num < 100
 }
+console.log(array.filter(filter))
