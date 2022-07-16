9e343bbcc601735fb98ffb0ab377b4f3e0b434bc
Ionella <ionellapb27@gmail.com>
2022-07-17 01:11:47 +0300

    git

--- fundamentals/Task5/index.js
+++ fundamentals/Task5/index.js
@@ -1,5 +1,21 @@
-function extractCurrencyValue(str){
-    return +str.slice(1);
+function square (value) {
+    return value * value;
 }
 
-alert( extractCurrencyValue('$120') === 120 );
+function double(value){
+    return 2 * value;
+}
+
+function triple(value){
+    return 3 * value;
+}
+
+function convert(fn, array){
+    return array.map(item => fn(item));
+}
+
+const initialArray = [1,2,3,4];
+
+console.log('initial: ', initialArray, 'square', convert(square, initialArray));
+console.log('initial: ', initialArray, 'double', convert(double, initialArray));
+console.log('initial: ', initialArray, 'triple', convert(triple, initialArray));
