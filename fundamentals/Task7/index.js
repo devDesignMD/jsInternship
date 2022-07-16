13b168653ce925f305a02fbcb6338a0f10f1d4b8
Ionella <ionellapb27@gmail.com>
2022-07-17 01:13:51 +0300

    git

--- fundamentals/Task7/index.js
+++ fundamentals/Task7/index.js
@@ -1,2 +1,5 @@
-if (!(age >= 14 && age <= 90));
-if (age <14 || age > 90);
+const arr =  [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(arr)
+console.log(arr.slice(0, 4))
+console.log(arr.slice(4, 7))
+console.log(arr.slice(7, 9))
