49f1f78ae95c77a3246e974b23850c80f12fafcf
Ionella <ionellapb27@gmail.com>
2022-07-17 00:19:03 +0300

    add directory

--- fundamentals/Task2/index.js
+++ fundamentals/Task2/index.js
@@ -1,1 +1,10 @@
-Task1
+const startInt= 22;
+const endInt= 42;
+let sum = 0;
+for (let i = startInt; i< endInt; i++) {
+    if (i % 2 === 0) {
+        sum += i;
+    }
+}
+
+console.log('suma: ', sum)
