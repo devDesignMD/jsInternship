2cab8bb39991f419d07eb959227b80c22f6e18f4
Ionella <ionellapb27@gmail.com>
2022-07-17 01:26:01 +0300

    git

---
+++ fundamentals/Task11/index.js
@@ -0,0 +1,7 @@
+let colors = ["white", "blue", "yellow", "black", "red", "green"];
+
+let [firstColor, secondColor, otherColors] = colors
+
+console.log(firstColor)
+console.log(secondColor)
+console.log(otherColors)
