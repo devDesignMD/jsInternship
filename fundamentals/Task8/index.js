9c66118b7a63fcff4d04ca68566aaf856da10627
Ionella <ionellapb27@gmail.com>
2022-07-17 00:28:24 +0300

    add directory

--- fundamentals/Task8/index.js
+++ fundamentals/Task8/index.js
@@ -1,11 +1,19 @@
-let value = prompt('Type the number', 0);
+let username = prompt("Who are you?", '');
 
-if (value > 0){
-    alert(1);
-}else if (value < 0){
-    alert(-1);
-}else if (value == 0){
-    alert(0);
-}else {
-    alert(Error)
+if (username == 'Admin'){
+
+    let password = prompt('Password?', '');
+
+    if (password == 'Boss'){
+        alert ('Hello');
+    }else if (password == '' || password == null ){
+        alert ('Cancel');
+    }else{
+        alert ('incorrect password');
+    }
+
+}else if (username == '' || username == null){
+    alert ('Cancel');
+}else{
+    alert ('incorrect username');
 }
