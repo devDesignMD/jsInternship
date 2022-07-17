295e96e20645ca227948167216b6a71dc618692d
Ionella <ionellapb27@gmail.com>
2022-07-17 15:35:23 +0300

    git

---
+++ fundamentals/Task12/index.js
@@ -0,0 +1,9 @@
+let person = {
+    first: 'Ion',
+    last: 'Creanga',
+    company: 'Amintiri din copilarie SRL'
+}
+    let {firstPerson, secondPerson, otherPerson} = person
+    console.log(firstPerson)
+    console.log(secondPerson)
+    console.log(otherPerson)
